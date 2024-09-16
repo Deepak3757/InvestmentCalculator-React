a18f83bb27dc4a8f70bebeac22e92a8a0573fb08
Deepak gupta <deepakgupta70363@gmail.com>
2024-09-16 22:03:44 +0530

    Adding the table below input window, hence Project almost completed

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,38 @@
-import { calculateInvestmentResults } from "../util/investment";
-export default function Results({userInput}){
-    const result=calculateInvestmentResults(userInput);
-    console.log(result);
+import { calculateInvestmentResults, formatter } from "../util/investment";
+export default function Results({ userInput }) {
+  const result = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    result[0].valueEndOfYear - result[0].interest - result[0].annualInvestment;
+  console.log(result);
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Investment Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {result.map(({ year, interest, valueEndOfYear, annualInvestment }) => {
+          const totalInterest =
+            valueEndOfYear -
+            annualInvestment * year -
+            initialInvestment;
+          const totalAmountInvested = valueEndOfYear - totalInterest;
+          return (
+            <tr key={year}>
+              <td>{year}</td>
+              <td>{formatter.format(valueEndOfYear)}</td>
+              <td>{formatter.format(interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
